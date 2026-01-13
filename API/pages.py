817f14ce083cbdfb5f680236aa037267797941dc
Nicklas Lykke Møller Jensen <Nicklaslmjensen@gmail.com>
2026-01-13 13:20:10 +0100

    initialize Flask application structure with routing and templates

---
+++ API/pages.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template, request
+
+
+
+bp = Blueprint("pages", __name__)
+
+
+@bp.route("/")
+def home():
+    
+
+
+    return render_template("pages/home.html")
